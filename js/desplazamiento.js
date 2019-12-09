81640583a39e8c48b2dee246cd27268eac939902
JimmyCanPoot <57239968+JimmyCanPoot@users.noreply.github.com>
2019-12-09 11:34:27 -0500

    AVANCES
    LO QUE HAY

---
+++ js/desplazamiento.js
@@ -0,0 +1,14 @@
+
+
+$('.banner a' ).click(function(e){				
+    e.preventDefault();		//evitar el eventos del enlace normal
+    var strAncla=$(this).attr('href'); //id del ancla
+        $('body,html').stop(true,true).animate({				
+            scrollTop: $(strAncla).offset().top
+        },1500);
+    
+});
+
+
+
+
